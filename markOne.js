1f267444c541fd456b04be6e089ea5055bc425eb
PranavMangalgi <73255252+PranavMangalgi@users.noreply.github.com>
2022-10-07 10:12:51 +0530

    upload code to repo
    uploaded code for the CLI app of markOne to Github

---
+++ markOne.js
@@ -0,0 +1,28 @@
+const prompt = require("prompt-sync")({ sigint: true });
+const playerName = prompt(`What is your name?`);
+
+console.log(`Welcome ${playerName}! Let's play 'how well do you know me'`);
+let score = 0;
+
+let arrayQuestions = [{ question: `Where do i live?`, answer: "bangalore" }, { question: `What am I studying?`, answer: `engineering` }, { question: `What is my favourite snack?`, answer: `pizza` }, { question: 'what is my favourite hobby?', answer: 'coding' }, { question: `What do i do in my free time?`, answer: `play football` }]
+
+
+play = (question, answer) => {
+  let playerAnswer = prompt(question);
+  if (playerAnswer === answer) {
+    console.log(`Correct answer!`);
+    score += 1;
+  }
+  else {
+    console.log(`Wrong answer :(`);
+  }
+  console.log(`Current score: ${score}`);
+  console.log(`______________`);
+  if (score === 5) {
+    console.log(`AWESOME! YOU GOT ALL THE 5 QUESTUONS RIGHT!`)
+  }
+}
+
+for (let i of arrayQuestions) {
+  play(i.question, i.answer);
+}  
